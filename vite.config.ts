1571e245d740eac0670de02b2b29ff8fd900da0e
Ethosa <social.ethosa@gmail.com>
2026-02-03 11:10:29 +0700

    fix: app base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss()
   ],
+  base: '/vue-accounts-entry-test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
